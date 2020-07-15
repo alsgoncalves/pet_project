640801fe468ebda8b1168accb5343ae042fffef4
Ana Goncalves <alsgoncalves91@gmail.com>
2020-07-15 11:02:49 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:update] do
+    resources :favorites, only: [:create, :destroy]
+    resources :participations, only: [ :index, :create, :destroy ]
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :organizations, only: [ :index, :show, :new, :create] do
+    resources :posts
+    resources :events
+  end
+
 end
