972598c531886404c08cf88d196bcc587e8f6402
Edgar Marques <edgar.eu1@hotmail.com>
2020-07-22 10:10:45 +0100

    Create the Favourite Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_084820) do
+ActiveRecord::Schema.define(version: 2020_07_22_090654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2020_07_22_084820) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["organization_id"], name: "ix_admin_by_org"
     t.index ["user_id", "organization_id"], name: "ix_admin_by_user_org", unique: true
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organization_id"], name: "index_favourites_on_organization_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -62,4 +71,6 @@ ActiveRecord::Schema.define(version: 2020_07_22_084820) do
 
   add_foreign_key "admins", "organizations"
   add_foreign_key "admins", "users"
+  add_foreign_key "favourites", "organizations"
+  add_foreign_key "favourites", "users"
 end
