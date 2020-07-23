09aa45748ae12e6ea5a6dcb18371352d24b1799e
Amanda Ciobanu <amanda@ciobanu.org>
2020-07-23 16:08:14 +0100

    add admin - form still not working

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,23 @@
 class AdminsController < ApplicationController
 
-  def is_owner?
-    @admin.is_owner = true
+  
+  def new
+    @organization = Organization.find(params[:organization_id])
+    @admin  = Admin.new
   end
+
+  def create
+    @organization = Organization.find(params[:organization_id])
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
