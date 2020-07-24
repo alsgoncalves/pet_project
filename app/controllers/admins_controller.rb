9a398b97f8ae24be8c2fcc22601ba3843981139d
Amanda Ciobanu <amanda@ciobanu.org>
2020-07-24 19:19:18 +0100

    form to add a new admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,11 +3,15 @@ class AdminsController < ApplicationController
   
   def new
     @organization = Organization.find(params[:organization_id])
+    @users = User.all
     @admin  = Admin.new
   end
 
   def create
     @organization = Organization.find(params[:organization_id])
+    @user_email = User.search(params[:email])
+    if @user_email.valid?
+    end
     
   end
 
