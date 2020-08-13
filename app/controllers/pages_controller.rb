3a7d09bbad8ec949c9b4a55dac5b380c5fa87f71
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-13 17:59:56 +0100

    Create the User feed div in the Feed page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,12 @@ class PagesController < ApplicationController
 
   def feed
     @name = "#{current_user.first_name} #{current_user.last_name}"
+
+    organizations_followed = current_user.favourites.map { |fav| fav.organization }
+
+    posts = organizations_followed.map { |org| org.posts }.flatten
+    events = organizations_followed.map { |org| org.events }.flatten
+
+    @feed = (posts + events).sort_by { |x| x.created_at }
   end
 end
