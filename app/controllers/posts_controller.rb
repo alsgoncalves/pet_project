e781d8c116c07891d9684b4c9f4e726510c0b373
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-17 23:52:10 +0100

    Change Posts View and Controller to take multiple images in Post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,6 +52,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description, :location, :date)
+    params.require(:post).permit(:title, :description, :location, :date, images: [])
   end
 end
