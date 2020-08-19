1ef8ae097f68602a4ce8a297bd0bdf7cb0cc57e2
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-19 22:43:03 +0100

    DRY the JS navbar pop-up code

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,14 +2,10 @@ const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
-  const avatar = document.querySelector('.nav-avatar img')
-  const avatarLinks = document.querySelector('.avatar-links')
+  const avatar = document.querySelector('.nav-avatar img');
+  const avatarLinks = document.querySelector('.avatar-links');
 
-  const burgerClick= burger.addEventListener('click', () => {
-    if (avatarLinks.classList.contains("avatar-links-active")) {
-      avatarLinks.classList.remove("avatar-links-active");
-    };
-
+  function burgerPopUpBehaviour() {
     // Toogle Nav
     nav.classList.toggle('nav-active');
 
@@ -19,35 +15,36 @@ const navSlide = () => {
         link.style.animation = ''
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-      }
+      };
     });
+
     // Burger animation
     burger.classList.toggle('cross');
+  };
+
+  const burgerClick = burger.addEventListener('click', () => {
+    if (avatarLinks.classList.contains("avatar-links-active")) {
+      avatarLinks.classList.remove("avatar-links-active");
+    };
+
+    burgerPopUpBehaviour();
   });
 
   const avatarClick = avatar.addEventListener('click', () => {
     if (nav.classList.contains("nav-active")) {
-      nav.classList.toggle("nav-active");
-
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = ''
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        }
-      });
+      burgerPopUpBehaviour();
     };
 
     avatarLinks.classList.toggle('avatar-links-active');
   });
-}
+};
 
 const NavbarOnScroll = () => {
   window.addEventListener('scroll', () => {
     const navbar = document.querySelector('.nav');
     navbar.classList.toggle('sticky', window.scrollY > 0);
   });
-}
+};
 
 export { NavbarOnScroll };
 
