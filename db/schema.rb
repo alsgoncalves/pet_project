190e9a6b9afe648d74a6ea55f77967c791de64e4
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-21 22:40:06 +0100

    Create the Category Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_145421) do
+ActiveRecord::Schema.define(version: 2020_08_21_213647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 2020_08_16_145421) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["organization_id"], name: "ix_admin_by_org"
     t.index ["user_id", "organization_id"], name: "ix_admin_by_user_org", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "events", force: :cascade do |t|
